391e353182eb0759f77045b91d4de1ae23ff369e
paloantom <72889414+paloantom@users.noreply.github.com>
2021-04-01 15:56:34 +0300

    Добавили Обертку Для базы данных (json)
    Co-Authored-By: ko-HakuRin <81675827+ko-HakuRin@users.noreply.github.com>

---
+++ src/Server/Db.cs
@@ -0,0 +1,37 @@
+﻿using Server.Types;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Server
+{
+    public class Db
+    {
+        public static List<Client> Clients { get; private set; }
+
+        public static void Instance()
+        {
+            if (File.Exists("client.json"))
+            {
+                Clients = JsonConvert.DeserializeObject<List<Client>>(File.ReadAllText("client.json"));
+                if (Clients == null)
+                    Clients = new List<Client>();
+            }
+            else
+            {
+                File.Create("client.json");
+                Clients = new List<Client>();
+            }
+        }
+
+        public static bool Exist(Client client)
+        {
+            return Clients.Exists(e => e.Name == client.Name && e.Password == client.Password);
+        }
+
+        public static void SaveToFile()
+        {
+            File.WriteAllText("client.json", JsonConvert.SerializeObject(Clients));
+        }
+    }
+}
---
+++ src/Server/Types/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Types
+{
+    public class Client
+    {
+        public string Name { get; }
+        public string Password { get; }
+
+        public Client(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
